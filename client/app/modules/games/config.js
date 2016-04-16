d0d0434f14fed10c5d62dc7d9cf6df97548deaf7
James McCray <chrisjamesjackmccray@gmail.com>
2016-04-15 20:49:35 -0500

    Commit

---
+++ client/app/modules/games/config.js
@@ -0,0 +1,10 @@
+function config($stateProvider) {
+  $stateProvider
+    .state('game',{
+      url: '/',
+      controller: "GameController as gameCtrl",
+      template: require('./view.html')
+    });
+}
+
+export default config;
---
+++ client/app/modules/games/controller.js
@@ -0,0 +1,11 @@
+class GameController {
+  constructor($http, $stateParams)
+  this._$http = $http;
+  this.$stateParams = $stateParams;
+  this.getData();
+
+}
+
+getData() {
+  this.gam
+}
