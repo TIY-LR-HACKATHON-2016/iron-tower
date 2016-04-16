ba1047e7b33c68200738b3b57435d86bb68bfe6c
Zach Ballard <zbginji@gmail.com>
2016-04-15 19:54:05 -0500

    Initial Commit of .net code.

---
+++ server/IronTower/IronTower.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IronTower.Web.Startup))]
+namespace IronTower.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
